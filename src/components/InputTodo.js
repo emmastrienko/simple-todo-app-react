6fecdeb9efa7fa1dd44f060a5df4440a8f0b3c31
Emma Strienko <mstrienko@gmail.com>
2024-09-09 20:21:45 +0300

    first commit

---
+++ src/components/InputTodo.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class InputTodo extends Component {
+  state = {
+    title: "",
+  };
+
+  onChange = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodoProps(this.state.title);
+    this.setState({
+      title: "",
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          value={this.state.title}
+          onChange={this.onChange}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default InputTodo;
